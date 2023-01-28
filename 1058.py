f1234d105ac067042c0b37c73b94c89e7b321173
MSJ99 <msj9518@gmail.com>
2023-01-28 17:06:12 +0900

    Solved in 23/01/08

--- 1058.py
+++ 1058.py
@@ -36,9 +36,3 @@ if __name__ == "__main__":
     for i in range(N):
         result.append(BFS(i))
     print(max(result))
-
-# 1  2   -> 2, 3
-# 2  1 3 -> 1, 3, 4
-# 3  2 4 -> 2, 4, 1, 5
-# 4  3 5 ->
-# 5  4   ->
