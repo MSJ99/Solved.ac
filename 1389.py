433bcbd274e4f183ac65b3d8f657dca01ee94950
MSJ99 <msj9518@gmail.com>
2023-01-29 19:03:40 +0900

    Solved in 23/01/29

---
+++ 1389.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+
+
+if __name__ == "__main__":
+    N = int(sys.stdin.readline())
+    graph = [[] for _ in range(N+1)]
+    M = int(sys.stdin.readline())
+    for _ in range(M):
+        A, B = map(int, sys.stdin.readline().split())
+        graph[A].append(B)
+        graph[B].append(A)
+
+    visited = [False] * (N+1)
+    q = deque()
+    q.append(1)
+    visited[1] = True
+    while q:
+        now = q.popleft()
+        for x in graph[now]:
+            if visited[x] == False:
+                q.append(x)
+                visited[x] = True
+
+    cnt = 0
+    for i in range(2, N+1):
+        if visited[i] == True:
+            cnt += 1
+    print(cnt)
